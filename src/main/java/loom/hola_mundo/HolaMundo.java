3f70a7468cd342b448ad217c7523ad8395e786a5
Brian Bayarri <38357877+briezeba@users.noreply.github.com>
2018-06-22 12:14:02 -0300

    Add files via upload

--- src/main/java/loom/hola_mundo/HolaMundo.java
+++ src/main/java/loom/hola_mundo/HolaMundo.java
@@ -1,27 +1,17 @@
 package loom.hola_mundo;
 
 /**
- * Clase Hola Mundo en Java.
+ * La clase HolaMundo.
  */
 public class HolaMundo {
 
-    /**
-     * Metodo saludar
+     /**
+     * Meotod Saludar.
      *
-     * @param El parametro persona indica el nombre de la persona a la que quiero saludar
-     * @return Se devuelve un String con el saludo 
+     * @param persona a la que saludaremos
+     * @return String saludando
      */
     public String saludar(final String persona) {
-            return "¡Hola, " + persona + "!";
-    }
-
-    /**
-     * Metodo saludar sin parametros
-     *
-     * @return Se devuelve un String con el saludo "Hola!"
-     */
-    public String saludar() {
-        return "¡Hola!";
-    }
+        return "¡Hola, " + persona + "!";
+     }
 }
-
